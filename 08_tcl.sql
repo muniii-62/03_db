e57a09e275151abbc735ffdded736c7758f26a34
tender0602 <munkyu0602@gmail.com>
2026-06-12 12:38:38 +0900

    20260612_ TCL,DDL

---
+++ 08_tcl.sql
@@ -0,0 +1,64 @@
+# TCL(Transactio Control Language)
+# 트랜잭션 제어 언어
+#COMMIT, ROLLBACK, SAVEPOINT
+
+#Transactino 이란?
+# 한번에 수행될 DML 논리적 작업 단위
+# 하나의 트랜잭션을 이용해서 관련 작업을
+# 한 번에 완료 또는 취소할 수 있게 하는 작업
+
+
+# =========
+#MySQL은 기본적으로 Autocommit 활성화 상태
+
+#MySQL은 기본적으로 Autocommit 활성화상태
+
+set autocommit = on; #활성화
+set autocommit = off; #비활성화
+
+#commit : DML로 인한 변경 사항(Transactino)을 DB에 반영
+#ROLLBACK : DML 변경 사항을 취소 (Transation 내부 내용 폐기)
+
+start transaction; # AUTOCOMMIT이 활성화 되어도 사용 가능
+# 트랜잭션 시작 == 이후 시행되는 DML 구문을 트랜잭션에 저장
+#트랜잭션 종료 = COMMIT, ROLLBACK
+
+select *
+from tbl_menu
+where menu_code = 21;
+
+#판매 가능 여부 Y -> N 수정
+update tbl_menu
+set orderable_status = 'N'
+where menu_code = 21;
+
+#수정 후 COMMIT을 수행하지 않았는데
+# 조회 시 수정 내용이 반영된 것 처럼 보이는 이유
+# -> 실제 DB에 반영은 되지 않았으나, 조회시 트랜잭션에 저장된 DML 구문을 반영해서 보여줌
+# 업데이트 전preview를 지원한다고 생각
+
+ROLLBACK;
+COMMIT;
+
+delete
+from tbl_menu
+where menu_code = 20;
+
+insert into tbl_menu
+values (null,
+        'TX테스트',
+        3000,
+        5,
+        'N');
+
+select *
+from tbl_menu;
+
+# menu.code = 100 삭제 후 DB 반영
+delete  from tbl_menu
+where menu_code= 100;
+
+commit;
+
+select* from tbl_menu;
+rollback;
